9dcc2216b900d20b072b7d0a91dfe0387ab40834
Pavel Pantus <ppantus@unison.com>
2014-10-16 10:39:47 -0400

    Showing additional info in case something went wrong

--- resign-ipa.sh
+++ resign-ipa.sh
@@ -18,7 +18,7 @@ cat << EOF
 	3rd - certificate.
 
 Example:
-./resign.sh Wiper.ipa Wiper_Appstore_Profile.mobileprovision "iPhone Distribution: Wiper, Inc."
+./resign-ipa.sh Wiper.ipa Wiper_Appstore_Profile.mobileprovision "iPhone Distribution: Wiper, Inc."
 
 Resulting ipa gets the same name + '-store' suffix.
 Result will be put next to input ipa.
@@ -26,69 +26,88 @@ Result will be put next to input ipa.
 EOF
 }
 
+fail()
+{
+    echo "*** Resigning failed ***"
+    exit 1
+}
+
 check_passed_parameter()
 {
+	echo "*** check_passed_parameter started ***"
 	if [[ $IPA_TO_RESIGN == *.ipa ]] && [ -f "$IPA_TO_RESIGN" ]
 	then
 		echo "Parameter passed verification."
 	else
 		echo "Bad parameter: it either doesn't have .ipa at the end or couldn't be found."
-		exit
+		exit 1
 	fi
+	echo "*** check_passed_parameter finished ***"
 }
 
 create_tmp_dir()
 {
+	echo "*** create_tmp_dir started ***"
 	if [ ! -d "$TMP_DIR_NAME_WITH_PATH" ]
 	then
-		mkdir $TMP_DIR_NAME_WITH_PATH
+		mkdir $TMP_DIR_NAME_WITH_PATH || fail
 	else
 		echo "Temporary dir with name $TMP_DIR_NAME_WITH_PATH already exists, please rename or delete it and start over."
-		exit
+		exit 1
 	fi
+	echo "*** create_tmp_dir finished ***"
 }
 
 remove_tmp_dir()
 {
+	echo "*** remove_tmp_dir started ***"
         if [ -d "$TMP_DIR_NAME_WITH_PATH" ]
 	then
-                rm -rf $TMP_DIR_NAME_WITH_PATH
+                rm -rf $TMP_DIR_NAME_WITH_PATH || fail
         else
 		echo "Couldn't delete temporary dir: it doesn't exist."
-                exit
+                exit 1
         fi
+	echo "*** remove_tmp_dir finished ***"
 }
 
 unzip_ipa()
 {
-	unzip -q $IPA_TO_RESIGN -d $TMP_DIR_NAME_WITH_PATH
+	echo "*** unzip_ipa started ***"
+	unzip -q $IPA_TO_RESIGN -d $TMP_DIR_NAME_WITH_PATH || fail
+	echo "*** unzip_ipa finished ***"
 }
 
 generate_ent()
 {
+	echo "*** generate_ent started ***"
 	APP_NAME=`ls $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/`
 
-	/usr/bin/codesign -d --entitlements :- $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME > $TMP_DIR_NAME_WITH_PATH/Entitlements.plist
+	/usr/bin/codesign -d --entitlements :- $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME > $TMP_DIR_NAME_WITH_PATH/Entitlements.plist || fail
+	echo "*** generate_ent finished ***"
 }
 
 replace_profile_and_sign()
 {
+	echo "*** replace_profile_and_sign started ***"
 	APP_NAME=`ls $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/`
 
-	rm -r $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME/_CodeSignature
-	cp "$PROFILE" $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME/embedded.mobileprovision
-	/usr/bin/codesign -f -s "$CERTIFICATE" --resource-rules $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME/ResourceRules.plist --entitlements $TMP_DIR_NAME_WITH_PATH/Entitlements.plist $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME
+	rm -r $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME/_CodeSignature || fail
+	cp "$PROFILE" $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME/embedded.mobileprovision || fail
+	/usr/bin/codesign -f -s "$CERTIFICATE" --resource-rules $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME/ResourceRules.plist --entitlements $TMP_DIR_NAME_WITH_PATH/Entitlements.plist $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME || fail
+	echo "*** replace_profile_and_sign finished ***"
 }
 
 zip_ipa()
 {
-	SHORT_VERSION=`/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME/Info.plist`
-	LONG_VERSION=`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME/Info.plist`
+	echo "*** zip_ipa started ***"
+	SHORT_VERSION=`/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME/Info.plist || fail`
+	LONG_VERSION=`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" $TMP_DIR_NAME_WITH_PATH/$UNZIPPED_DIR_NAME/$APP_NAME/Info.plist || fail`
 
-	PREV_PWD=$PWD
-	cd $TMP_DIR_NAME_WITH_PATH
-	zip -qr $PREV_PWD/Wiper-$SHORT_VERSION-$LONG_VERSION-store.ipa $UNZIPPED_DIR_NAME
-	cd $PREV_PWD
+	pushd $TMP_DIR_NAME_WITH_PATH || fail
+	zip -qr $PATH_TO_IPA_TO_RESIGN/Wiper-$SHORT_VERSION-$LONG_VERSION-store.ipa $UNZIPPED_DIR_NAME || fail
+	popd
+	echo "*** zip_ipa finished ***"
 }
 
 if [[ $# -eq 0 ]]; then
